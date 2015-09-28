58f1a0dd41b18840b85cbd1bccf23f20a3fbc1e5
Ckai1991 <1610721416@qq.com>
2015-09-28 16:47:41 +0800

    Initial commit

---
+++ src/main/java/org/smart4j/framework/util/ArrayUtil.java
@@ -0,0 +1,27 @@
+package org.smart4j.framework.util;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by Ckai on 15/9/24.
+ */
+public final class ArrayUtil {
+    /**
+     * 判断数组是否非空
+     */
+    public static boolean isNotEmpty(Object[] array) {
+        return !ArrayUtils.isEmpty(array);
+    }
+
+    /**
+     * 判断数组是否为空
+     */
+    public static boolean isEmpty(Object[] array) {
+        return ArrayUtils.isEmpty(array);
+    }
+}
